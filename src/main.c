b900e12eee396d9028db03da6546435c65c9744c
Paddi <paddipaddi@pm.me>
2026-03-02 20:18:00 +0100

    file opening/creation handling

--- src/main.c
+++ src/main.c
@@ -19,6 +19,9 @@ int main(int argc, char *argv[]) {
     bool newfile = false;
     int c;
 
+    // database file descriptor
+    int dbfd = -1;
+
     while ((c = getopt(argc, argv, "nf:")) != -1) {
         switch (c) {
             case 'n':
@@ -38,6 +41,22 @@ int main(int argc, char *argv[]) {
         print_usage(argv);
         return 0;
     }
+
+    if (newfile) {
+        dbfd = create_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to create database file\n");
+            return -1;
+        }
+    } else {
+        dbfd = open_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to open database file\n");
+            return -1;
+        }
+    }
+
+    close_db_file(dbfd);
 
     printf("newfile: %d\n", newfile);
     printf("filepath: %s\n", filepath);
