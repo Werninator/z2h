686c55e1a635fdd7319c19d660b8db47bdf89b90
Paddi <paddipaddi@pm.me>
2026-03-02 19:44:32 +0100

    accept the first flags

--- src/main.c
+++ src/main.c
@@ -11,6 +11,30 @@ void print_usage(char *argv[]) {
 
 }
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[]) {
+    char *filepath = NULL;
+    bool newfile = false;
+    int c;
 
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+            case 'n':
+                newfile = true;
+                break;
+            case 'f':
+                filepath = optarg;
+                break;
+            case '?':
+                printf("Unknown option -%c\n", c);
+                break;
+            default:
+                printf("Something weird happened..\n");
+                return -1;
+        }
+    }
+
+    printf("newfile: %d\n", newfile);
+    printf("filepath: %s\n", filepath);
+
+    return 0;
 }
